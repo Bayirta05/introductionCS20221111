d91dc168ae7dc0ea189a112e312b469b6ff53b91
bayrata <ivanov@example.com>
2022-11-15 16:47:02 +0300

    Lesson 1

---
+++ 12/Program.cs
@@ -0,0 +1,13 @@
+﻿//12. С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+System.Console.WriteLine("Введите число:");
+int N=Convert.ToInt32(Console.ReadLine());
+int a=N%10;
+int b=N/10;
+if (a>b)
+{
+    System.Console.WriteLine($"{a} наибльшая цифра числа {N}");
+}
+else
+{
+    System.Console.WriteLine($"{b} наибольшая цифра числа {N}");
+}
