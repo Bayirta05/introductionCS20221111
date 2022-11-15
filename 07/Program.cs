ed391c2335739324bec717dc75caa2f98decf115
bayrata <ivanov@example.com>
2022-11-15 16:12:02 +0300

    Lesson 1

---
+++ 07/Program.cs
@@ -0,0 +1,11 @@
+﻿//7. Выяснить является ли число чётным.
+System.Console.WriteLine("Введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n%2==0)
+{
+    System.Console.WriteLine($"{n} - это четное число");
+}
+else
+{
+    System.Console.WriteLine($"{n} - это нечетное число");
+}
