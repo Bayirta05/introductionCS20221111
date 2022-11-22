1f7e0d55277166cd5617d22e4402d9db8ce01367
bayrata <ivanov@example.com>
2022-11-22 09:32:32 +0300

    Lesson 3

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,2 +1,10 @@
 ﻿//25. Вывести на экран кубы чисел от 1 до N
 
+int N=Convert.ToInt32(Console.ReadLine());
+int i=1;
+while(i<=N)
+{
+    int c=(int)Math.Pow(i,3);
+    System.Console.WriteLine($"Куб числа {i} равен {c}");
+    i++;
+}
