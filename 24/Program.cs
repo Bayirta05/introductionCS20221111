c5f11bd7a16527e921400a3bfff0d21a5d9c6d49
bayrata <ivanov@example.com>
2022-11-23 21:03:58 +0300

    Lesson 3

---
+++ 24/Program.cs
@@ -0,0 +1,8 @@
+﻿//24. Вывести на экран таблицу квадратов чисел от 1 до N
+int N=Convert.ToInt32(Console.ReadLine());
+int square=0;
+for(int i=1;i<=N;i++)
+{
+    square=i*i;
+    System.Console.WriteLine($"{i} * {i} = {square}");
+}
