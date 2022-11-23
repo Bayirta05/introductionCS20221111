abfa6f651891285020901c1ac4b7ef652a7a0dbe
bayrata <ivanov@example.com>
2022-11-23 20:10:22 +0300

    Lesson 3

---
+++ 30/Program.cs
@@ -0,0 +1,12 @@
+﻿//30. Написать программу вычисления произведения чисел от 1 до N
+int Multi(int Number)
+{
+    int a=1;
+    for(int i=1;i<=Number;i++)
+    {
+        a*=i;
+    }
+    return a;
+}
+int n=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Multi(n));
