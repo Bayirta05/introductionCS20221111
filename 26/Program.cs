8682732118ccb70012483cc1dd59fd3afd91c726
bayrata <ivanov@example.com>
2022-11-23 21:19:47 +0300

    Lesson 3

---
+++ 26/Program.cs
@@ -0,0 +1,12 @@
+﻿//26. Найти сумму чисел от 1 до А
+int Summa(int Number)
+{
+    int result=0;
+    for(int i=1;i<=Number;i++)
+    {
+        result+=i;
+    }
+    return result;
+}
+int A=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Summa(A));
