b3621f4fd6d4c795f93e45a56853d202ee90b3f9
bayrata <ivanov@example.com>
2022-11-23 19:31:51 +0300

    Lesson 3

---
+++ 27/Program.cs
@@ -0,0 +1,9 @@
+﻿//27. Возведите число А в натуральную степень B используя цикл
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int result=a;
+for(int i=1;i<b;i++)
+{
+    result*=a;
+}
+System.Console.WriteLine(result);
