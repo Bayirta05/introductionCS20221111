eb49dbf9ca5f540bb87f386107034e41910f1dc8
bayrata <ivanov@example.com>
2022-11-26 23:32:56 +0300

    Lesson 4

--- basic_algorithm/05-switch/Program.cs
+++ basic_algorithm/05-switch/Program.cs
@@ -2,3 +2,30 @@
 
 int day=1;
 if (day==1) System.Console.WriteLine("Понедельник");
+
+switch(day)
+{
+    case 1:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Среда");
+        break;
+    default:
+        System.Console.WriteLine("Неправильный ввод");
+        break;
+}
+
+metka1:
+
+goto metka1; //оператор безусловного перехода
+//GOTO -нельзя использовать
+
+void Test()
+{
+    metka2:
+        System.Console.WriteLine();
+}
