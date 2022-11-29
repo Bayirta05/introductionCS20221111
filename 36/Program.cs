176dffbcc3bf1d02782039b1007389f9e8113cb8
bayrata <ivanov@example.com>
2022-11-29 21:43:40 +0300

    Lesson 4

---
+++ 36/Program.cs
@@ -0,0 +1,38 @@
+﻿//36. Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] RandomIntArray(int size=10,int min=0,int max=7)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max);
+    return a;
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int NumSearch(int[] m,int find,int startIndex=0)
+{
+    int i=startIndex;
+    while(i<m.Length && m[i]!=find) i++;
+    if (i==m.Length)
+        return -1;
+    else
+        return i;
+}
+
+
+int[] m=RandomIntArray();
+int find=6;
+Print(m);
+System.Console.WriteLine();
+int i=NumSearch(m,find);
+if (i==-1)
+    System.Console.WriteLine($"Числа {find} нет в массиве");
+else
+    System.Console.WriteLine($"Число {find} присутствует");
