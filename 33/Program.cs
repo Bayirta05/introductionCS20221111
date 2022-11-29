a9b5fcb7b06c70c92f0540f6576d6ad3ad725a8b
bayrata <ivanov@example.com>
2022-11-29 20:17:02 +0300

    Lesson 4

---
+++ 33/Program.cs
@@ -0,0 +1,22 @@
+﻿//33. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void RandomIntArray(out int[] a, int size=8,int min=0,int max=1)
+{
+    a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(0,2);    
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+
+
+int[] m;
+RandomIntArray(out m);
+Print(m);
