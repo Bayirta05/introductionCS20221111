caef4e20a08d4eec9f4edb796a3ced1e213c13ce
bayrata <ivanov@example.com>
2022-11-29 23:02:50 +0300

    Lesson 4

--- 41/Program.cs
+++ 41/Program.cs
@@ -1,29 +1,13 @@
-﻿//41. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+﻿//33. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
-int[] RandomIntArray(int size=10,int min=0,int max=100)
+void RandomIntArray(out int[] a, int size=8,int min=0,int max=1)
 {
-    int[] a=new int[size];
+    a=new int[size];
     Random random=new Random();
     for(int i=0;i<size;i++)
-        a[i]=random.Next(min,max+1);
-    return a;
+        a[i]=random.Next(0,2);    
 }
 
-int Max(int[] a)
-{
-    int m=a[0];
-    for(int i=1;i<a.Length;i++)
-         if (a[i]>m) m=a[i];
-    return m;
-}
-
-int Min(int[] a)
-{
-    int m=a[0];
-    for(int i=1;i<a.Length;i++)
-         if (a[i]<m) m=a[i];
-    return m;
-}
 
 void Print(int[] a)
 {
@@ -31,7 +15,8 @@ void Print(int[] a)
         System.Console.Write($"{a[i],5}");
 }
 
-int[] a=RandomIntArray(3,0,10);
-Print(a);
-System.Console.WriteLine();
-System.Console.WriteLine(Max(a)-Min(a));
+
+
+int[] m;
+RandomIntArray(out m);
+Print(m);
--- 43/Program.cs
+++ 43/Program.cs
@@ -9,19 +9,21 @@ int[] RandomIntArray(int size=10,int min=0,int max=100)
     return a;
 }
 
-void Change(int[] a)
-{
-    for(int i=0;i<a.Length;i++)
-        System.Console.Write($"{a[i]*(-1),5}");
-}
-
 void Print(int[] a)
 {
     for(int i=0;i<a.Length;i++)
         System.Console.Write($"{a[i],5}");
 }
 
+void Change(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        a[i]=a[i]*(-1);
+}
+
 int[] a=RandomIntArray(10,-5,5);
 Print(a);
 System.Console.WriteLine();
 Change(a);
+System.Console.WriteLine();
+Print(a);
