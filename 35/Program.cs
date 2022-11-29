c399a58c150f2d4c66c3578c5c9cf748035bd066
bayrata <ivanov@example.com>
2022-11-29 21:25:02 +0300

    Lesson 4

---
+++ 35/Program.cs
@@ -0,0 +1,27 @@
+﻿//35. Написать программу замены элементов массива на противоположные
+
+int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+
+void Change(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i]*(-1),5}");
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int[] a=RandomIntArray(10,-5,5);
+Print(a);
+System.Console.WriteLine();
+Change(a);
