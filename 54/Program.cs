4a82a4da77fd975236360544418ad7ec48d615f5
bayrata <ivanov@example.com>
2022-12-02 22:44:22 +0300

    Lesson 5

---
+++ 54/Program.cs
@@ -0,0 +1,10 @@
+﻿//54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+int N=Convert.ToInt32(Console.ReadLine());
+int[] a=new int[N];
+a[0]=0;
+a[1]=1;
+for(int i=2;i<N;i++)
+    a[i]=a[i-1]+a[i-2];
+for(int i=0;i<N;i++)
+    System.Console.Write($"{a[i]} ");
