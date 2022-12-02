3b6a06428c2b9366838f05652bee3a60e0d68e41
bayrata <ivanov@example.com>
2022-12-02 22:24:39 +0300

    Lesson 5

---
+++ 51/Program.cs
@@ -0,0 +1,22 @@
+﻿//51. С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+int[] ArrayNum(int Number)
+{
+    int[] a=new int[Number];
+    for(int i=0;i<a.Length;i++)
+        a[i]=Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int CountNum(int[] a)
+{
+    int count=0;
+    for(int i=0;i<a.Length;i++)
+        if (a[i]>0) count+=1;
+    return count;
+}
+
+int N=Convert.ToInt32(Console.ReadLine());
+int[] a=ArrayNum(N);
+System.Console.WriteLine();
+System.Console.WriteLine(CountNum(a));
